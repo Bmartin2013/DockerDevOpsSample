0e900d7b662f53f721dcd675d1d72e06b0b9acef
Barbara Martinez <barbara.martinez@southworks.com>
2020-08-13 13:12:50 -0300

    Change greeting

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hi!\n');
+  res.end('Hello from velocity team!\n');
 });
 
 server.listen(port, hostname, () => {
